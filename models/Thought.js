eae408b74bc4e01e2f97b9a6bc013ed43bb43a2d
In Code We Trust <njderenne@gmail.com>
2021-01-15 20:16:33 -0600

    added functionality to user-model with routes and controller

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            //add from the user
+        },
+        reactions: []//add to this in future from reactionSchema
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
